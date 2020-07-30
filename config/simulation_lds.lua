0a377013f9965eddcab5a084ed66c10a339d1eec
Ali Muneer <amuneer@ualberta.ca>
2020-07-30 17:35:34 -0600

    edit sh file'

--- config/simulation_lds.lua
+++ config/simulation_lds.lua
@@ -19,12 +19,12 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "velodyne",
-  published_frame = "velodyne",
+  tracking_frame = "base_laser_link",
+  published_frame = "base_laser_link",
   odom_frame = "odom",
   provide_odom_frame = true,
   publish_frame_projected_to_2d = false,
-  use_odometry = false,
+  use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
   num_laser_scans = 1,
@@ -32,7 +32,7 @@ options = {
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
-  submap_publish_period_sec = 0.3,
+  submap_publish_period_sec = 0.2,
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
@@ -44,18 +44,18 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 35
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 150
 TRAJECTORY_BUILDER_2D.min_range = 0.3
 TRAJECTORY_BUILDER_2D.max_range = 8.
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 1.
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.3
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 10.
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 1e-1
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 10
 
 POSE_GRAPH.optimization_problem.huber_scale = 1e2
-POSE_GRAPH.optimize_every_n_nodes = 35
+POSE_GRAPH.optimize_every_n_nodes = 25
 POSE_GRAPH.constraint_builder.min_score = 0.65
 
 return options
---
+++ simulator_2d_run.sh
@@ -0,0 +1,5 @@
+cp config/simulation_2d.rviz ../install_isolated/share/cartographer_ros/configuration_files/
+cp config/simulation_lds.lua ../install_isolated/share/cartographer_ros/configuration_files/
+
+roslaunch launch/simulator.launch
+
