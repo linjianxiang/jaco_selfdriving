b195777534d8be5a13f8ce51a86cf94cd6630581
linjianxiang <l.jxiang@hotmail.com>
2020-07-31 01:55:47 -0600

    move_base launch

--- jaco_2d_run.sh
+++ jaco_2d_run.sh
@@ -1,5 +1,5 @@
 cp config/jaco_2d.rviz ../install_isolated/share/cartographer_ros/configuration_files/
 cp config/jaco_lds.lua ../install_isolated/share/cartographer_ros/configuration_files/
 
-roslaunch launch/jaco.launch bag_filename:=/home/lab/cato_ws/map/rosbags/jaco_bag.bag
+roslaunch launch/jaco_bag.launch bag_filename:=/home/linjian/workspaces/cartographer_ws/jaco_selfdriving/rosbags/jaco_bag.bag
 
